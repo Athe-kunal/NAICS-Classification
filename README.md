2766f2588f9867a78b389d0eefc9082f9a91fd1e
Athe-kunal <athekunal@gmail.com>
2024-07-18 15:46:43 -0400

    frontend build up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NAICS-Classification
+
+This is a NAICS classification Agent that can get the NAICS Code from the question and classify it. It uses GLiNER and Vector Database from OpenAI or Sentence transformer to build the vector database and find relevant industries and their NAICs code.
--- frontend.py
+++ frontend.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import re
 from annotated_text import annotated_text, annotation
 from app.prediction import Prediction
 from app.vectordb import load_database
+from typing import List, Union, Tuple
 
 st.title("NAICS Classification")
 
@@ -13,8 +15,34 @@ def load_resources():
 
 prediction = load_resources()
 
+def process_question(mod_question:str)->List[Union[str,Tuple[str]]]:
+    """Get annotated words for streamlit view
+
+    Args:
+        mod_question (str): Question to be modified
+
+    Returns:
+        _type_: _description_
+    """
+    for spans, inames in zip(ner_entities_span,industry_names_code):
+        curr_end = spans[1]
+        ner = f"#-{inames.industry}:{inames.naics_code}-# "
+        mod_question = mod_question[:curr_end] + ner + mod_question[curr_end+1:]
+    pattern = r'(#[^#]*#)|\s+'
+
+    # Splitting the sentence based on the pattern
+    chunks = [chunk for chunk in re.split(pattern, mod_question) if chunk and not chunk.isspace()]
+    annotated_words = []
+
+    for chunk in chunks:
+        if "#-" in chunk and "-#" in chunk:
+            annotated_words[-1] = (annotated_words[-1],chunk[2:-2])
+        else:
+            annotated_words.append(chunk+" ")
+    return annotated_words
+
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Hi, how can I help you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "NAICS Classification Agent"}]
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -32,16 +60,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
         with st.spinner("Answering..."):
             ner_entities_span, industry_names_code = prediction.pipeline(question)
             # response = f"Entities: {ner_entities_span}\nIndustry Names: {industry_names_code}"
-            annotated_words = [f"{qs} " for qs in question.split(" ")]
-            for spans, inames in zip(ner_entities_span,industry_names_code):
-                curr_start = spans[0]
-                curr_end = spans[1]
-                # get the NER words
-                ner = question[curr_start:curr_end].split(" ")
-                for aw in annotated_words:
-                    if aw.strip() in ner:
-                        aw = (aw,f"{inames.industry}: {inames.naics_code}")
-            response = annotated_text(tuple(annotated_words))
-            st.write(response)
-    message = {"role": "assistant", "content": response}
+            annotated_words = process_question(question)
+            st.write(annotated_text(annotated_words))
+    message = {"role": "assistant", "content": annotated_text(annotated_words)}
     st.session_state.messages.append(message)
